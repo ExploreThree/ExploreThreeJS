01c5bc7529a901afffa62081e1ff8d79eace0359
Kevin Charles <char0042@umn.edu>
2019-11-22 17:03:52 -0600

    Dashed Line Triangle

--- src/App.js
+++ src/App.js
@@ -35,12 +35,14 @@ class App extends Component {
       0.1, // near plane
       1000 // far plane
     );
-    this.camera.position.z = 5; // is used here to set some distance from a cube that is located at z = 0
+    this.camera.position.z = 10; // is used here to set some distance from a cube that is located at z = 0
+    // this.camera.position.x = 10; // is used here to set some distance from a cube that is located at z = 0
     // OrbitControls allow a camera to orbit around the object
     // https://threejs.org/docs/#examples/controls/OrbitControls
     this.controls = new OrbitControls(this.camera, this.el);
     this.renderer = new THREE.WebGLRenderer({antialias:true});
-    this.renderer.setClearColor("#0000FF");
+    // this.renderer = new THREE.WebGLRenderer({});
+    this.renderer.setClearColor("#FFFFFF");
     this.renderer.setSize(width, height);
     this.el.appendChild(this.renderer.domElement); // mount using React ref
   };
@@ -49,43 +51,75 @@ class App extends Component {
   // Code below is taken from Three.js BoxGeometry example
   // https://threejs.org/docs/#api/en/geometries/BoxGeometry
   addCustomSceneObjects = () => {
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x00ff00,
-      emissive: 0x072534,
-       side: THREE.DoubleSide,
-      flatShading: true
-    });
+    // const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // const material = new THREE.MeshPhongMaterial({
+    //   color: 0x00ff00,
+    //   emissive: 0x072534,
+    //    side: THREE.DoubleSide,
+    //   flatShading: true
+    // });
    
-    this.cube = new THREE.Mesh(geometry, material);
-    this.cube.translateX(3);
+    // this.cube = new THREE.Mesh(geometry, material);
+    // this.cube.translateX(3);
 
-    this.scene.add(this.cube);
+    // this.scene.add(this.cube);
 
-    const geometry2 = new THREE.SphereGeometry(2,3,3);
-    const material2 = new THREE.MeshLambertMaterial({color:0xFFCC00});
+    // var heartShape = new THREE.Shape();
 
-    this.sphere = new THREE.Mesh(geometry2, material2);
-    this.sphere.translateX(-3);
-    this.scene.add(this.sphere);
+    // heartShape.moveTo( 25, 25);
+    
+    // heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+    // heartShape.bezierCurveTo( -30, 0, -30, 35,-30,35 );
+    // heartShape.bezierCurveTo( -30, 55, -10, 77, 25, 95 );
+    // heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+    // heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+    // heartShape.bezierCurveTo( 35, 0, 0, 25, 25, 25 );
 
-    const lights = [];
-    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    // var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 8, bevelSize: 1, bevelThickness: 1 };
 
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
+    // let heartGeometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
 
-    this.scene.add(lights[0]);
-    this.scene.add(lights[1]);
-    this.scene.add(lights[2]);
+    // this.heart = new THREE.Mesh(heartGeometry,new THREE.MeshPhongMaterial({color: 0xff0000}));
+    // this.scene.add(this.heart);
+
+
+    // var material = new THREE.LineBasicMaterial( { color: 0x000000 } );
+    var material = new THREE.LineDashedMaterial( {
+      color: 0x000000,
+      linewidth: 1,
+      scale: 1,
+      dashSize: 1.5,
+      gapSize: 2,
+    } );
+
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
+    geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
+    geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+    geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
+
+    this.line = new THREE.Line( geometry, material );
+    this.line.computeLineDistances();
+    // this.line.width = 50;
+    this.scene.add(this.line);
+
+    // const lights = [];
+    // lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+    // lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    // lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+
+    // lights[0].position.set(0, 200, 0);
+    // lights[1].position.set(100, 200, 100);
+    // lights[2].position.set(-100, -200, -100);
+
+    // this.scene.add(lights[0]);
+    // this.scene.add(lights[1]);
+    // this.scene.add(lights[2]);
   };
 
   startAnimationLoop = () => {
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    // this.cube.rotation.x += 0.01;
+    // this.cube.rotation.y += 0.01;
 
     this.renderer.render(this.scene, this.camera);
 
