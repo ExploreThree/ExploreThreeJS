824e09e5641fb4f475eb082c93caf8a00e7d0288
Kevin Charles <char0042@umn.edu>
2020-03-16 23:44:26 -0500

    Simpler handleWindowResize

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ class App extends Component {
     window.removeEventListener("resize", this.handleWindowResize);
     window.cancelAnimationFrame(this.requestID);
     // this.controls.dispose();
+    this.dragContols.dispose();
+    this.renderer.dispose();
   }
 
   // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
@@ -50,19 +52,21 @@ class App extends Component {
     //   1000 // far plane
     // );
     this.aspectRatio = this.width/this.height;
-    this.viewSize = 20;
+    this.viewSize = .05;
     this.camera = new THREE.OrthographicCamera(
+      -this.width*this.viewSize / 2, //left
+      this.width*this.viewSize / 2, //right
       // -this.aspectRatio*this.viewSize / 2, //left
       // this.aspectRatio*this.viewSize / 2, //right
-      -this.aspectRatio*this.viewSize / 2, //left
-      this.aspectRatio*this.viewSize / 2, //right
-      this.viewSize / 2, //top
-      -this.viewSize / 2, //bottom
+      this.height*this.viewSize / 2, //top
+      -this.height*this.viewSize / 2, //bottom
       -1000, //near
       1000 //far
     );
     this.camera.zoom = 1;
     this.camera.position.z = 10; // is used here to set some distance from a cube that is located at z = 0
+    // this.camera.aspect = this.width / this.height;
+
     // this.camera.position.x = 10; // is used here to set some distance from a cube that is located at z = 0
     // OrbitControls allow a camera to orbit around the object
     // https://threejs.org/docs/#examples/controls/OrbitControls
@@ -110,37 +114,36 @@ class App extends Component {
 
   handleWindowResize = () => {
     
-    let priorArea = this.width * this.height;
+
     this.width = this.el.clientWidth;
     this.height = this.el.clientHeight;
-    this.viewSize = this.viewSize * priorArea / (this.width * this.height);
-    this.aspectRatio = this.width/this.height;
-
-    this.camera.left = -this.aspectRatio*this.viewSize / 2;
-    this.camera.right = this.aspectRatio*this.viewSize / 2;
-    this.camera.top = this.viewSize / 2;
-    this.camera.bottom = this.viewSize / -2;
     
-    this.renderer.setSize(this.width, this.height);
-    // this.camera.aspect = width / height;
+    this.camera.left = -this.width*this.viewSize / 2; 
+    this.camera.right = this.width*this.viewSize / 2; 
+    this.camera.top = this.height*this.viewSize / 2;
+    this.camera.bottom = -this.height*this.viewSize / 2;
+    
+    // this.camera.aspect = this.width / this.height;
     
 
     // Note that after making changes to most of camera properties you have to call
     // .updateProjectionMatrix for the changes to take effect.
     this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.width, this.height);
+
     this.forceUpdate();
   };
 
   render() {
 
-    var w = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
+//     var w = window.innerWidth
+// || document.documentElement.clientWidth
+// || document.body.clientWidth;
 
-var h = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
-    console.log(`${w} x ${h}` );
+// var h = window.innerHeight
+// || document.documentElement.clientHeight
+// || document.body.clientHeight;
+    // console.log(`${w} x ${h}` );
     
 
     let width = window.innerWidth;
